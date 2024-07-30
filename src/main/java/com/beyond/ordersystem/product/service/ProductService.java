13c96b61f58914d3f30090d66bc9334f03d2856e
tteia <tteia.dl@gmail.com>
2024-07-30 11:13:42 +0900

    AWS 를 통한 이미지 파일 업로드 확인
    ProductService.productAwsCreate 를 통해 첨부한 product 이미지가 s3 를 통해 DB 에 저장됨을 확인.

--- src/main/java/com/beyond/ordersystem/product/service/ProductService.java
+++ src/main/java/com/beyond/ordersystem/product/service/ProductService.java
@@ -5,11 +5,16 @@ import com.beyond.ordersystem.product.dto.ProductResDto;
 import com.beyond.ordersystem.product.dto.ProductSaveReqDto;
 import com.beyond.ordersystem.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -23,11 +28,17 @@ import java.util.UUID;
 @Service
 @Transactional
 public class ProductService{
-    public final ProductRepository productRepository;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final ProductRepository productRepository;
+    private final S3Client s3Client;
 
     @Autowired
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, S3Client s3Client) {
         this.productRepository = productRepository;
+        this.s3Client = s3Client;
     }
 
     public Product productCreate(ProductSaveReqDto createDto){
@@ -58,9 +69,21 @@ public class ProductService{
         try {
             product = productRepository.save(createDto.toEntity());
             byte[] bytes = image.getBytes();
-            Path path = Paths.get("/Users/tteia/Desktop/tmp/", product.getId() + "_" + image.getOriginalFilename());
+            String fileName = product.getId() + "_" + image.getOriginalFilename();
+            Path path = Paths.get("/Users/tteia/Desktop/tmp/", fileName);
+
+            // local pc 에 임시 저장.
             Files.write(path, bytes, StandardOpenOption.CREATE, StandardOpenOption.WRITE);
-            product.updateImagePath(path.toString());
+
+            // aws 에 pc 저장 파일을 업로드.
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(fileName)
+                    .build();                                                                      // 실습 시 확인을 위해 .fromFile 해줬는데 .fromByte 로 바로 해줘도 됨 !
+            PutObjectResponse putObjectResponse = s3Client.putObject(putObjectRequest, RequestBody.fromFile(path));
+            String s3Path = s3Client.utilities().getUrl(a->a.bucket(bucket).key(fileName)).toExternalForm(); // 우리 파일 이름이 아니라 s3 관련 이름으로 나옴.
+            product.updateImagePath(s3Path);
+            // https://tteia-file.s3.ap-northeast-2.amazonaws.com/A1.png
         } catch (IOException e) {
             throw new RuntimeException("이미지 저장 실패 !"); // 트랜잭션 처리를 위해 예외 잡아주기
         }
